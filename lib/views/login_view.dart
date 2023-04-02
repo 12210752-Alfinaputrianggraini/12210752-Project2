fe9842da76d8c77fc09720a45b6f9d80a6e07f94
12210752-Alfinaputrianggraini <12210752@bsi.ac.id>
2023-04-02 22:32:34 +0700

    login view

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -6,69 +6,71 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 100, bottom: 20),
-            child: Text(
-              'Aplikasi\nPendataan Warga',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 100, bottom: 20),
+              child: Text(
+                'Aplikasi\nPendataan Warga',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-          Stack(
-            children: [
-              Container(
-                height: 380,
-                child: Card(
-                  margin: EdgeInsets.only(top: 50, left: 20, right: 20),
-                  color: Colors.blue,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 100,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              fillColor: Colors.white,
-                              filled: true),
+            Stack(
+              children: [
+                Container(
+                  height: 380,
+                  child: Card(
+                    margin: EdgeInsets.only(top: 50, left: 20, right: 20),
+                    color: Colors.blue,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 100,
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              fillColor: Color.fromARGB(255, 255, 255, 255),
-                              filled: true),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                fillColor: Colors.white,
+                                filled: true),
+                          ),
                         ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 40),
-                          child: Text('Login'),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                fillColor: Color.fromARGB(255, 255, 255, 255),
+                                filled: true),
+                          ),
                         ),
-                        style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30)),
-                            primary: Colors.red),
-                      )
-                    ],
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 40),
+                            child: Text('Login'),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30)),
+                              primary: Colors.red),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Center(
-                  child: Image.asset(
-                'assets/logo.png',
-                width: 120,
-              )),
-            ],
-          ),
-        ],
+                Center(
+                    child: Image.asset(
+                  'assets/logo.png',
+                  width: 120,
+                )),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
